de7132df5fdbdd985e93b81ee2bf78ee66dea8bc
Vraj Joshi <vrajj@glidemtech.com>
2022-01-21 11:13:48 +0530

    Removed build files

--- examples/connectivity/ble_connectivity/pca10040/ser_s132_hci/ses/Output/Release/Obj/ble_connectivity_s132_hci_pca10040/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/Release/Obj/ble_connectivity_s132_hci_pca10040/ses_startup_nrf_common.o: \
- D\:\New\ folder\xdps-connectivity-firmware-test\modules\nrfx\mdk\ses_startup_nrf_common.s
--- examples/connectivity/ble_connectivity/pca10040/ser_s132_hci/ses/Output/Release/Obj/ble_connectivity_s132_hci_pca10040/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/Release/Obj/ble_connectivity_s132_hci_pca10040/thumb_crt0.o: \
- C\:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.50d/source/thumb_crt0.s
